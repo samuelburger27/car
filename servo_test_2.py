765dd75a9546a40df316a72127ad41e3695c6e2f
silaksila <silaksila27@gmail.com>
2022-01-09 13:01:07 +0100

    k

--- servo_test_2.py
+++ servo_test_2.py
@@ -3,13 +3,13 @@ import time
 
 servo = gpiozero.AngularServo(
     14,
-    initial_angle=0,
+    initial_angle=0, min_angle= -45, max_angle= 45
     min_pulse_width=(1 - 0.45) / 1000,
     max_pulse_width=2.45 / 1000,
 )
 
 print("STARting")
-time.sleep(2)
+time.sleep(4)
 servo.angle = 25
 print("YAAA")
 time.sleep(2)
