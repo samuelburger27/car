224f997efb54f388e6a61a86331e69993bbaf4fa
silaksila <silaksila27@gmail.com>
2022-07-09 21:23:25 +0200

    fvv

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,24 @@
 
+import imp
 from flask import Flask
 from markupsafe import escape
 from flask import render_template
+from requests import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def loadout():
-    return render_template('loadout.html')
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'Do Something':
+            print("SOMETHING ELSE")
+        elif request.form['submit_button'] == 'Do Something Else':
+            print("SOMETHING ELSE")
+        else:
+            pass  # unknown
+    elif request.method == 'GET':
+        return render_template('loadout.html')
 
 
 @app.route('/start_car/')
